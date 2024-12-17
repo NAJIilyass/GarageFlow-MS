9dd01cdaf38d9b3d63ebf234fa59c7769cb415f7
IlYess <najiilyassoo@gmail.com>
2024-12-18 00:00:54 +0100

    Fix POST and PATCH requests issue in the API Gateway

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -5,9 +5,9 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
 
 app.use("/", require("./routes/apiGatewayRoutes"));
+
 app.listen(process.env.PORT, () =>
     console.log(`API Gateway listening on port ${process.env.PORT}`)
 );
